2e8e4e7923c54c15a7906d9a0517c4a8200bf3a3
Muhammad Munir <mmunir1214@gmail.com>
2025-07-30 02:07:57 -0400

    Setting up Home Page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "HireFit" },
+    { name: "description", content: "Know Your Fit. Get the Job That’s Lit." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className={"main-section"}>
+      <div className={"page-heading"}>
+        <h1>Application Analytics That Work for You</h1>
+        <h2>
+          Track applications, get feedback, and apply with confidence.
+        </h2>
+      </div>
+    </section>
+  </main>
 }
