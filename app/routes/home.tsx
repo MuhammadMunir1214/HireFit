3e59b429b5d3c8d600f7a5e342b650b55ef633f6
Muhammad Munir <mmunir1214@gmail.com>
2025-07-31 03:12:19 -0400

    Added resume components on Homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
-
+import Navbar from "../components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,14 +11,24 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <section className={"main-section"}>
-      <div className={"page-heading"}>
-        <h1>Application Analytics That Work for You</h1>
-        <h2>
-          Track applications, get feedback, and apply with confidence.
-        </h2>
-      </div>
-    </section>
-  </main>
+  return (
+      <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+        <Navbar />
+
+        <section className="main-section">
+          <div className="page-heading">
+            <h1>Application Analytics That Work for You</h1>
+            <h2>Track applications, get feedback, and apply with confidence.</h2>
+          </div>
+        </section>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </main>
+  );
 }
